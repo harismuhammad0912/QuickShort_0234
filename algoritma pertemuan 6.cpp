85a850617e3e4bd68b3d357607ab413243f133c0
harismuhammad0912 <muhammadharis091204@gmail.com>
2024-03-28 15:42:50 +0700

    17

--- algoritma pertemuan 6.cpp
+++ algoritma pertemuan 6.cpp
@@ -100,3 +100,7 @@ void display() {
     {
         cout << arr[i] << " ";
     }
+
+ cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+    cout << "Number of data movements: " << mov_count << endl;
+}
